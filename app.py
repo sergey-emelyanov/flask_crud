2ea3904fbf74f04a2094f22498135e29ad4c223e
sergey-emelyanov <emelia35@yandex.ru>
2023-03-27 20:48:33 +0300

    add users path

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+from validate import validation
 import uuid
 import json
 
@@ -19,9 +20,25 @@ def show_users():
     return render_template("/users/index.html", users=users)
 
 
+@app.route("/user/<string:id>")
+def show_user(id):
+    with open("data_file.json", "r") as read_file:
+        data = json.load(read_file)
+
+    user = [x for x in data['users'] if x['id'] == id][0]
+
+    return render_template("/users/show.html", user=user)
+
+
 @app.route("/users/new")
 def new_user():
-    return render_template("/users/new_user.html")
+    user = {
+        "id": "",
+        "name": "",
+        "email": ""
+    }
+    errors = {}
+    return render_template("/users/new_user.html", errors=errors, user=user)
 
 
 @app.post("/users")
@@ -29,10 +46,13 @@ def post_users():
     user = request.form.to_dict()
     user_id = uuid.uuid4()
     new_user = {
-        "id": str(user_id),
+        "id": str(user_id)[:8],
         "name": user["name"],
         "email": user["email"]
     }
+    errors = validation(user)
+    if errors:
+        return render_template("/users/new_user.html", errors=errors, user=new_user)
     with open("data_file.json", "r", encoding="utf-8") as read_file:
         data = json.load(read_file)
         data["users"].append(new_user)
